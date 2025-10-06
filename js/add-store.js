c0f4db4dbe4f41d46504f7dab3d378c10b1c9d80
worldcigarlocator-maker <231854619+worldcigarlocator-maker@users.noreply.github.com>
2025-10-06 21:04:28 +0200

    Update add-store.js

--- js/add-store.js
+++ js/add-store.js
@@ -1,4 +1,4 @@
-const supabaseUrl = "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"; // TODO: byt till din riktiga Supabase URL
+const supabaseUrl = "https://gbxxoeplkzbhsvagnfsr.supabase.co"; // TODO: byt till din riktiga Supabase URL
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdieHhvZXBsa3piaHN2YWduZnNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NjQ1MDAsImV4cCI6MjA3MzI0MDUwMH0.E4Vk-GyLe22vyyfRy05hZtf4t5w_Bd_B-tkEFZ1alT4"; // TODO: byt till din riktiga anon key
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
