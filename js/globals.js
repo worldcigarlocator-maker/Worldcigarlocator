0a80bc4dc7a6dac3f727aeeea1eb6684f972a8c2
worldcigarlocator-maker <worldcigarlocator@gmail.com>
2025-11-21 23:36:07 +0100

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://gbxxoeplkzbhsvagnfsr.supabase.co",
-  "YOUR_PUBLIC_ANON_KEY"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdieHhvZXBsa3piaHN2YWduZnNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NjQ1MDAsImV4cCI6MjA3MzI0MDUwMH0.E4Vk-GyLe22vyyfRy05hZtf4t5w_Bd_B-tkEFZ1alT4"
 );
 
 export const qs = (id) => document.getElementById(id);
