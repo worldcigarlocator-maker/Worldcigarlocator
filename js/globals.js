1d578e8802007e160a36a358759d130f17b11f59
worldcigarlocator-maker <worldcigarlocator@gmail.com>
2026-02-07 12:59:50 +0100

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -3,7 +3,8 @@
 // Frontend-only · ESM · Stable CDN
 // ============================================================
 
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.94.1/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.94.1";
+
 
 /* ============================================================
    SUPABASE CLIENT
