c778f87ed235db1a1705b3a3cf548b49511f75d7
Andreas Bagler <worldcigarlocator@gmail.com>
2026-03-05 09:53:46 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -26,10 +26,8 @@ async function loadGoogleMaps() {
   await new Promise((resolve, reject) => {
 
     const script = document.createElement("script");
-
-    script.src =
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBzHH9QNHPGWpQrczIGgWs1wnHGALiwNZw&v=weekly&libraries=marker";
-
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyBzHH9QNHPGWpQrczIGgWs1wnHGALiwNZw&v=weekly&libraries=marker";
     script.async = true;
     script.onload = resolve;
     script.onerror = reject;
