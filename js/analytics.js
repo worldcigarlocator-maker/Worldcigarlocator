c236994ce6edf546bd09bfafd9aef15515905797
Andreas Bagler <worldcigarlocator@gmail.com>
2026-03-10 22:48:09 +0100

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   await loadStoresIndex();
   await loadGlobalKpis();
   await renderOverview();
-   await loadTrafficFlow();
+  await loadTrafficFlow();
 });
 
 /* ============================================================
