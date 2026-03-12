e9ca1dffdff2affb1fd2482001e554e925817355
Andreas Bagler <worldcigarlocator@gmail.com>
2026-03-12 23:48:34 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -240,19 +240,4 @@ if (clearBtn) {
 
 }
 
-/* ============================================================
-   MOBILE SIDEBAR BRAND
-   ============================================================ */
 
-@media (max-width:768px){
-
-  .sidebar-brand svg{
-    width:180px;
-    height:auto;
-  }
-
-  .sidebar-brand text{
-    font-size:34px;
-  }
-
-}
