e49daf89afafeb00b9f412ffa04dec5a7097774d
Andreas Bagler <worldcigarlocator@gmail.com>
2026-05-13 00:03:09 +0200

    Update funnel-users.js

--- js/funnel-users.js
+++ js/funnel-users.js
@@ -118,8 +118,8 @@ setActiveDay(day);
 
     // 🔥 RENDER
     const days = Number(document.getElementById("globalRange")?.value || 30);
-    const m = await import("./funnel-market.js");
-    await m.renderMarket(days);
+ const m = await import("./market-v2.js");
+await m.renderMarketV2(days);
 
   };
 
